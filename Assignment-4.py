3b30770bfc635ca4b4339c8d1442abdc00997353
vamsibyramla <70326727+vamsibyramla@users.noreply.github.com>
2020-11-15 19:53:20 +0530

    Update Assignment-4.py

--- Assignment-4.py
+++ Assignment-4.py
@@ -4,6 +4,7 @@
 # In[3]:
 
 
+#question-1
 import pandas as pd
 import numpy as np
 print(pd.__version__)
@@ -12,6 +13,7 @@ print(pd.__version__)
 # In[51]:
 
 
+#question-2:
 a = np.array(['vamsi', 'sai', 'Althaf', 'Rakesh'])
 s = pd.Series(a)
 print(s)
@@ -20,6 +22,7 @@ print(s)
 # In[56]:
 
 
+#question-3:
 a = np.array(['vamsi', 'sai', 'Althaf', 'Rakesh'])
 s = pd.Series(a)
 print(s)
@@ -31,6 +34,7 @@ print(df1)
 # In[78]:
 
 
+#question-4:
 import seaborn as sns
 print(sns.get_dataset_names())
 df2 = sns.load_dataset('mpg')
@@ -47,6 +51,7 @@ print(df2['origin'].unique())
 # In[84]:
 
 
+#question-6
 usa = (df2['origin'] == 'usa')
 df_usa = df2[usa]
 print(df_usa)
